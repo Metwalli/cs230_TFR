b926001f37baef89754dee5e567ce0675563fcd6
Metwalli <metwalli0@gmail.com>
2019-04-06 11:39:54 +0800

    update the project to read TFRecord files

--- model/model_fn.py
+++ model/model_fn.py
@@ -73,7 +73,7 @@ def model_fn(mode, inputs, params, reuse=False):
         # Compute the output distribution of the model and the predictions
         logits = build_model(is_training, inputs, params)
         # logits = SmallerVGGNet.build(is_training, inputs, params)
-        # logits = DenseNet(x=inputs, params=params, reuse=reuse, is_training=is_training).model
+        logits = DenseNet(x=inputs, params=params, reuse=reuse, is_training=is_training).model
         predictions = tf.argmax(logits, 1)
 
     # Define loss and accuracy
